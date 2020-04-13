6e91021ccef53730adedcddb90f9a57175958889
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-13 18:00:44 -0300

    Exibindo texto com printf

--- modulo1/src/introducao/PrimeiroPrograma.java
+++ modulo1/src/introducao/PrimeiroPrograma.java
@@ -5,6 +5,9 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome\nto\nJava\nProgramming!");
+		
+		System.out.printf("%s%n%s%n",
+				"Welcome to", "Java Programming!");
 
 	}
 
