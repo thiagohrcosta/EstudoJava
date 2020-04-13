d2d049e6013010550049412ae2c16244db210239
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-13 17:58:56 -0300

    Exibindo múltiplas linhas de texto com uma única instrução

--- modulo1/src/introducao/PrimeiroPrograma.java
+++ modulo1/src/introducao/PrimeiroPrograma.java
@@ -4,8 +4,7 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 
-		System.out.print("Hello World. ");
-		System.out.println("Welcome to Java Programming!");
+		System.out.println("Welcome\nto\nJava\nProgramming!");
 
 	}
 
