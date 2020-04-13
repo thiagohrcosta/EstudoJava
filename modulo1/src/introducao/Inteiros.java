351a1504baf50bc1e4d56ee63d4e4931bce7daa7
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-13 18:05:53 -0300

    Trabalhando com Inteiros

---
+++ modulo1/src/introducao/Inteiros.java
@@ -0,0 +1,30 @@
+package introducao;
+import java.util.Scanner;
+
+public class Inteiros {
+
+	public static void main(String[] args) {
+		
+		// Criar um Scanner para obter entrada a partir de um comando
+		Scanner sc = new Scanner(System.in);
+		
+		int number1;
+		int number2;
+		int sum;
+		
+		System.out.print("Digite o primeiro inteiro:");
+		number1 = sc.nextInt();
+		
+		System.out.print("Digite o segundo inteiro:");
+		number2 = sc.nextInt();
+		
+		sum = number1 + number2;
+		
+		System.out.printf("O resultado da soma é %d%n ", sum);
+		
+
+		
+
+	}
+
+}
