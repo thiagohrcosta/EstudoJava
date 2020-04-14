73d3345b133cc649d28cbbe2010b0e666b3a2190
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-14 20:52:13 -0300

    Finalizando Exercício de expectativa de crescimento mundial

--- modulo1/src/introducao/ExercicioFixacao3.java
+++ modulo1/src/introducao/ExercicioFixacao3.java
@@ -1,5 +1,7 @@
 package introducao;
 
+import java.text.DecimalFormat;
+
 public class ExercicioFixacao3 {
 
 	public static void main(String[] args) {
@@ -7,7 +9,25 @@ public class ExercicioFixacao3 {
 		// a taxa de crescimento demográfico mundial anual. Escreva um aplicativo que introduza esses valores e, então, 
 		// que exiba a população mundial estimada depois de um, dois, três, quatro e cinco anos
 
+		DecimalFormat f = new DecimalFormat("##");
+
+
+		double taxaCrescimento = 0.11;
+		double popMundial = 7.7;
 		
+		double ano;
+		
+		int anoAtual = 2020;
+		
+		for (ano = 1; ano <= 5; ano++) {
+			double expectativaMundial = (popMundial * taxaCrescimento) * 10;
+			popMundial = expectativaMundial;
+			String resultado = String.format("%.2f", expectativaMundial);
+
+			anoAtual = anoAtual + 1;
+			
+			System.out.println("A expectativa é que a população mundial alcance em " + anoAtual + " o total de " + resultado + " bilhões de habitantes.");
+		}
 		
 		}
 		
