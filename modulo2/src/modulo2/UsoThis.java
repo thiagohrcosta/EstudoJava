f1eb592db9593cce1f18e787d6d09c782b72c08c
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-17 15:50:30 -0300

    Classe, método set e get

---
+++ modulo2/src/modulo2/UsoThis.java
@@ -0,0 +1,14 @@
+package modulo2;
+
+public class UsoThis {
+
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
