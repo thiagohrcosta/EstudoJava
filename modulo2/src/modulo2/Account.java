2392ade142b8de3ea038849cdfa3600d380dc0c9
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-17 15:56:42 -0300

    Classe AccountTest condutora

---
+++ modulo2/src/modulo2/Account.java
@@ -0,0 +1,15 @@
+package modulo2;
+
+public class Account {
+	
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+}
---
+++ modulo2/src/modulo2/AccountTest.java
@@ -0,0 +1,27 @@
+package modulo2;
+
+import java.util.Scanner;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		Account myAccount = new Account();
+		
+		System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+		
+		System.out.println("Please enter the name:");
+		String theName = input.nextLine();
+		myAccount.setName(theName);
+
+		System.out.println();
+		
+		System.out.printf("Name in objetc myAccount is:%n%s%n",
+				myAccount.getName());
+		input.close();
+
+	}
+
+}
